72272d2a06c2871e36b66e23516eb2c33ba55c41
chenxiao07150808 <932900547@qq.com>
2020-03-16 10:46:30 +0800

    springboot +redis

---
+++ src/test/java/com/chenxiao/newsdemo/NewsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenxiao.newsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
